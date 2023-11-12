de94397846d2fac431c2ac8ba910dd8c8533e6a8
SamahZughaibi <sama7.1999@gmail.com>
2023-11-12 17:22:04 +0300

    implemented getAllCoffees in CoffeeService

--- src/app/services/coffee.service.ts
+++ src/app/services/coffee.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class CoffeeService {
 
-  constructor() { }
+export class CoffeeService {
+  private readonly API_URL = "https://ih-coffees-api-42e1df06199a.herokuapp.com/coffees";
+
+  constructor(private http: HttpClient) { }
+
+  getAllCoffees(): Observable<any>{
+    return this.http.get<any>(this.API_URL);
+  }
 }
